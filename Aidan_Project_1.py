57eba810566c7d8ec32b86f53904d905c3b6e3ee
ALWCormier <alwc935e@gmail.com>
2017-07-11 17:00:08 -0400

    Arbitrary Population Data

---
+++ Aidan_Project_1.py
@@ -0,0 +1,96 @@
+# Using pandas
+
+# setup
+import pandas as pd
+df = pd.read_csv('Sample_Dataset.csv')
+# mean
+df['Total'].mean()
+# median
+df['Total'].median()
+# mode
+df['Total'].mode()
+# range
+df['Total'].max()-df['Total'].min()
+
+
+# Using NumPy
+
+# setup
+import numpy as np
+df = np.genfromtxt('Sample_Dataset.csv', delimiter = ',', usecols = (2,3))
+df = np.delete(df, (0), axis=0)
+# mean
+df.mean(axis=0)
+# medium
+np.median(df, axis=0)
+# mode
+from scipy import stats
+stats.mode(df,axis=0)
+# range
+np.amax(df, axis=0) - np.amin(df,axis=0)
+
+
+# In Basic Python
+
+# setup
+import pyexcel as p
+sheet = p.get_sheet(file_name="Sample_Dataset.csv")
+del sheet.row[0]
+total = sheet.column[2]
+total = list(total)
+native = sheet.column[3]
+native = list(native)
+# mean
+total_mean = float(sum(total))/float(len(total)) #mean total
+native_mean = float(sum(native))/float(len(native)) #mean native
+# median
+total.sort()
+native.sort()
+thalf = len(total)/2 
+thalf = int(thalf)
+if thalf % 2 == 0:
+ total_med = total[thalf] + total[thalf-1]
+ total_med = total_med/2
+else:
+ total_med = total[thalf] 
+print(total_med) # median total
+if thalf % 2 == 0:
+ native_med = native[thalf] + native[thalf-1]
+ native_med = native_med/2
+else:
+ native_med = native[thalf]
+print(native_med) # median native
+# range
+total.sort()
+native.sort()
+r = len(total)
+r = r-1 
+r = int(r)
+total_range = total[r] - total[0] # total range
+native_range = native[r] - native[0] # native range
+# mode total
+store_tot = []
+tot_mode = []
+for item in total:
+ if store_tot == []:
+  store_tot = total
+ else:
+  total = store_tot
+ store1 = item 
+ total.remove(item)
+ for number in total:
+  if store1 == number:
+   tot_mode.append(number) # total mode
+# mode native
+store_nat = []
+nat_mode = []
+for item in native:
+ if store_nat == []:
+  store_nat = native
+ else:
+  native = store_nat
+ store2 = item 
+ native.remove(item)
+ for number in native:
+  if store2 == number:
+   nat_mode.append(number) # native mode
